65257120d47c9d9cb4c5710ea584817b1a8605ee
Babken <inantsbabken@gmail.com>
2022-10-30 21:23:09 +0400

    Brilliants Added

--- Assets/Scripts/PlayerCollisions.cs
+++ Assets/Scripts/PlayerCollisions.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerCollisions : MonoBehaviour
 {
@@ -10,13 +11,13 @@ public class PlayerCollisions : MonoBehaviour
     public PlayerController PlayerControllerScript;
     public GameObject Blocker;
     public Timer TimerCanvas;
+    public GameObject SkinCanvas;
+    public GameObject SettingsCanvas;
+    public GameObject HTPCanvas;
+    public Text BrilliantText;
+    
+    private int Brilliant;
     private Timer TimerScript;
-    [SerializeField]
-    private GameObject SkinCanvas;
-    [SerializeField]
-    private GameObject SettingsCanvas;
-    [SerializeField]
-    private GameObject HTPCanvas;
 
     
     private void OnCollisionEnter(Collision collisioninfo)
@@ -40,11 +41,20 @@ public class PlayerCollisions : MonoBehaviour
             TimerScript.Stop();
             int scene = SceneManager.GetActiveScene().buildIndex - 1;
             PlayerPrefs.SetInt("Level" + scene + "Complete", 1);  
+            PlayerPrefs.SetInt("Brilliants", Brilliant);
+        }
+        
+        //Brilliants
+        if (collisioninfo.collider.tag == "Brilliant")
+        {
+            Brilliant++;
+            Destroy(collisioninfo.collider.gameObject);
         }
     }
 
     private void Update()
     {
+        BrilliantText.text = Brilliant.ToString();
         if(Input.GetKeyDown(KeyCode.Escape) && SettingsCanvas.activeSelf)
         {
             SettingsCanvas.SetActive(false);
